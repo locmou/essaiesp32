75770b411fc9d6c0e17418fbc87ac9f1dc61757c
locmou <loic.mounier@laposte.net>
2025-12-29 22:28:21 +0100

    Essai dht11

--- main.cpp
+++ main.cpp
@@ -10,28 +10,19 @@
 
 /** Initialize DHT sensor 1 */
 DHTesp dhtSensor1;
-/** Initialize DHT sensor 2 */
-DHTesp dhtSensor2;
-/** Initialize DHT sensor 3 */
-DHTesp dhtSensor3;
+
 /** Task handle for the light value read task */
 TaskHandle_t tempTaskHandle = NULL;
 /** Pin number for DHT11 1 data pin */
-int dhtPin1 = 17;
-/** Pin number for DHT11 2 data pin */
-int dhtPin2 = 16;
-/** Pin number for DHT11 3 data pin */
-int dhtPin3 = 27;
+int dhtPin1 = 32;
+
 /** Ticker for temperature reading */
 Ticker tempTicker;
 /** Flags for temperature readings finished */
 bool gotNewTemperature = false;
 /** Data from sensor 1 */
 TempAndHumidity sensor1Data;
-/** Data from sensor 2 */
-TempAndHumidity sensor2Data;
-/** Data from sensor 3 */
-TempAndHumidity sensor3Data;
+
 
 /* Flag if main loop is running */
 bool tasksEnabled = false;
@@ -51,8 +42,7 @@ void tempTask(void *pvParameters) {
 			// Reading temperature for humidity takes about 250 milliseconds!
 			// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
 			sensor1Data = dhtSensor1.getTempAndHumidity();	// Read values from sensor 1
-			sensor2Data = dhtSensor2.getTempAndHumidity();	// Read values from sensor 1
-			sensor3Data = dhtSensor3.getTempAndHumidity();	// Read values from sensor 1
+
 			gotNewTemperature = true;
 		}
 		vTaskSuspend(NULL);
@@ -79,10 +69,7 @@ void setup() {
 
 	// Initialize temperature sensor 1
 	dhtSensor1.setup(dhtPin1, DHTesp::DHT11);
-	// Initialize temperature sensor 2
-	dhtSensor2.setup(dhtPin2, DHTesp::DHT11);
-	// Initialize temperature sensor 3
-	dhtSensor3.setup(dhtPin3, DHTesp::DHT11);
+
 
 	// Start task to get temperature
 	xTaskCreatePinnedToCore(
@@ -113,12 +100,9 @@ void setup() {
  */
 void loop() {
 	if (gotNewTemperature) {
-		Serial.println("Sensor 1 data:");
-		Serial.println("Temp: " + String(sensor1Data.temperature,2) + "'C Humidity: " + String(sensor1Data.humidity,1) + "%");
-		Serial.println("Sensor 2 data:");
-		Serial.println("Temp: " + String(sensor2Data.temperature,2) + "'C Humidity: " + String(sensor2Data.humidity,1) + "%");
-		Serial.println("Sensor 3 data:");
-		Serial.println("Temp: " + String(sensor3Data.temperature,2) + "'C Humidity: " + String(sensor3Data.humidity,1) + "%");
+		Serial.println("Sensor data: ");
+		Serial.print("Temp: " + String(sensor1Data.temperature,2) + "'C Humidity: " + String(sensor1Data.humidity,1) + "%");
+
 		gotNewTemperature = false;
 	}
 } // End of loop
