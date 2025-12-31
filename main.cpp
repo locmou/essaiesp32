b216822057a531b31e933c4495d4baf52616b1ea
locmou <124572531+locmou@users.noreply.github.com>
2025-12-31 18:08:13 +0100

    Introduction d'une led témoin de la luminosité et void rétroéclairage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-
-
 #include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
 #include <LiquidCrystal_I2C.h>
 
@@ -10,19 +8,29 @@ DHTesp dhtSensor;
 // ---------- CONFIG LCD ----------
 LiquidCrystal_I2C lcd(0x27, 20, 4); // Adapter l'adresse I2C (0x27 ou 0x3F)
 
-// Définir la broche 33 comme entrée analogique
-const int photoresistorPin = 33;
-
 /** Pin number for DHT11 1 data pin */
-const int dhtPin = 32;
+const uint8_t dhtPin = 32;
 
-/**
- * Arduino setup function (called once after boot/reboot)
- */
+//Définition des contrastes
+const uint8_t BRIGHTNESS_PIN=5;   // Must be a PWM pin
+// Définir la broche 33 comme entrée analogique
+const uint8_t LDR = 33;
+const uint8_t bright;
+
+// Ajuste le rétroéclairage en fonction de la mesure de luminosité ambiante
+void Retroeclairage()
+  {//réglage de l'intensité lumineus du LCD selon la lumière ambiante
+  bright=(analogRead(LDR)/4);
+  analogWrite(BRIGHTNESS_PIN, 255-bright);}
+
+
 void setup() {
 
 	Serial.begin(115200);
 	Serial.println("Lecture DHT11 + affichage LCD");
+
+//Déclaration de la Led LCD
+pinMode(BRIGHTNESS_PIN, OUTPUT);
 
 	// LCD
   	lcd.init();
